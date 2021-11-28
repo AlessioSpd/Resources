5679ab0ba0b96ebe01af6721e02cc3d32a34406c
Alessio <pamuimc@gmail.com>
2021-11-28 15:34:43 +0100

    new version

--- Python/Directory creator.py
+++ Python/Directory creator.py
@@ -1,17 +1,73 @@
+import os, shutil
 import os
+import pathlib
 
-n = int(input("Number of dir: "))
-path = input("New dir path: ")
-comm = "mkdir " + path
-i = 0
-nameList = []
+def menu():
+	print("1. Numbered")
+	print("2. For each file here")
+	print("3. Help")
+	print("Any other key to exit")
 
-while i < n:
-	nome = input("Name: ")
-	nameList.append(nome)
-	i = i + 1
+	return int(input())
 
-i = 0
-while i < n:
-	os.system(comm+"\\"+nameList[i])
-	i = i + 1
+def numbered(myPath : str):
+	nDir = int(input("\nHow many dir? "))
+	n = 1
+
+	while n <= nDir:
+		os.makedirs(myPath + "\\" + str(n))
+		n = n + 1
+	
+	return True
+
+def filed(myPath : str):
+	#input del tipo di file che cerco
+	typeOfFile = str(input("\nWhich type of file? "))
+
+	#prendo una lista dei file con l'estensione scelta
+	arr = os.listdir(myPath)
+
+	myFile = []
+
+	#prendo tutti i nomi dei file che hanno l'estensione inserita
+	for file in arr:
+		if file.endswith(typeOfFile):
+			myFile.append(file)
+
+	#creo le cartelle con i nomi dei file
+	numero = 0
+	while numero < len(myFile):
+		os.makedirs(myPath + "\\" + myFile[numero][:-4])
+		numero = numero + 1
+
+	#sposto ogni file nella sua omonima cartella
+	numero = 0
+	while numero < len(myFile):
+		shutil.move(myPath + "\\" + myFile[numero], myPath + "\\" + myFile[numero][:-4])
+		numero = numero + 1
+
+	return True
+
+def takePath():
+	myPath = str(input("Input path:"))
+	if myPath == "this":
+		myPath = os.path.dirname(os.path.realpath(__file__))
+	return myPath
+
+def main():
+	myPath = takePath()
+	exit = False
+	while exit == False:
+		ris = menu()
+		if ris == 1:
+			exit = numbered(myPath)
+		elif ris == 2:
+			exit = filed(myPath)
+		elif ris == 3:
+			print("\nSe premi 1, puoi creare N cartelle numerate,\naltrimenti, se premi 2, puoi creare N cartella da una specifica estensione dei file, copiando il suo nome e spostandoli al suo interno\n")
+		else:
+			exit()
+
+	input("\nAll good. Press Enter to continue...")
+
+main()
